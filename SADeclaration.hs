11d97da1c7a4edc38d03434f6c5a9095a78ced33
Alex Teiche <alexteiche@gmail.com>
2015-04-29 21:18:28 -0700

    Finished checkExprDeclarations.  It checks for use before declaration, and multiple declarations of the same symbol in one scope.

--- SADeclaration.hs
+++ SADeclaration.hs
@@ -6,21 +6,31 @@ import Data.Maybe
 
 import Syntax
 
-data SemanticError = UseBeforeDeclaration
+data SemanticError = UseBeforeDeclaration Name
                    | MultipleDeclarations [Symbol]
                      deriving (Show)
             
 type Scope = [Symbol]
 type SymbolTable = [Scope]
 
+--------------------------------
+-- Symbol Table Manipulations --
+--------------------------------
+    
+visible :: SymbolTable -> Name -> Bool
+visible st name = name `elem` visibleNames
+  where visibleNames = map symbolName (concat st)
+
 -- Given a symbol, determine if it's in the current scope    
-inScope :: SymbolTable -> Symbol -> Bool
-inScope st sym = sym `elem` (head st)
+inTopScope :: SymbolTable -> Symbol -> Bool
+inTopScope st sym = sym `elem` (head st)
     
 -- Given a symbol, look it up in the symbol table    
 getBySymbol :: SymbolTable -> Symbol -> Maybe Symbol
 getBySymbol st sym = listToMaybe $ filter (((==) `on` symbolName) sym) (head st)
-    
+
+
+                     
 -- Return a list of lists of elements that are duplicates as determined by f
 dupsBy :: (a -> a -> Bool) -> [a] -> [[a]]
 dupsBy f = filter ((>1) . length) . groupBy f
@@ -37,19 +47,28 @@ checkDeclarations exprs = checkDeclarations' [] exprs
 checkDeclarations' :: SymbolTable -> [Expr] -> [SemanticError]
 checkDeclarations' st exprs = concatMap (checkExprDeclarations st) exprs
 
--- Perform analysis on a single expression                              
+-- Analyze a single expression for declaration issues, and recurse into child notes
+-- FuncDef:
+---- At a function definition we enter a new scope
+---- So get all symbols defined as arguments, make sure their aren't duplicates,
+---- and recurse into child nodes
+-- VariableDecl:
+---- When a new symbol is declared, make sure it doesn't exist in the current scope
 checkExprDeclarations :: SymbolTable -> Expr -> [SemanticError]
 checkExprDeclarations st (BinOp _ left right) = (checkExprDeclarations st left) ++
                                                 (checkExprDeclarations st right)
--- At a function definition we enter a new scope
--- So get all symbols defined as arguments, make sure their aren't duplicates,
--- and recurse into child nodes
 checkExprDeclarations st (FuncDef _ _ args children) = localErrors ++ (checkDeclarations'
-                                                                          (args : st) children)
+                                                                       (args : st) children)
   where localErrors = errorDuplicates args
 checkExprDeclarations st (FuncCall _ exprs) = checkDeclarations' st exprs
--- When a new symbol is declared, make sure it doesn't exist in the current scope
 checkExprDeclarations st (VariableDecl sym) = case (getBySymbol st sym) of
                                                 Nothing -> []
                                                 Just s -> [MultipleDeclarations [sym, s]]
-checkExprDeclarations _ _ = []                                                          
+checkExprDeclarations st (If cond body) = (checkExprDeclarations st cond) ++
+                                            (checkDeclarations' st body)
+checkExprDeclarations st (Const _) = []
+checkExprDeclarations st (Assignment e1 e2) = checkDeclarations' st [e1, e2]
+checkExprDeclarations st (Identifier name) = if (visible st name) then [] else
+                                                 [UseBeforeDeclaration name]
+checkExprDeclarations st (Return (Just expr)) = checkExprDeclarations st expr
+checkExprDeclarations _ _ = []
