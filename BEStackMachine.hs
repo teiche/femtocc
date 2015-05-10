d69c189b582d6aead25dbcf4450fa77d5d842049
Alex Teiche <alexteiche@gmail.com>
2015-05-10 12:13:11 -0700

    Settled on a calling convention and added function calls

--- BEStackMachine.hs
+++ BEStackMachine.hs
@@ -2,6 +2,7 @@
 
 module BEStackMachine where
 
+import Data.List (intercalate)
 import Data.Maybe (fromJust)    
 import Numeric
     
@@ -13,11 +14,13 @@ import FrameOffsetTable
 
 data Operand = Reg Int
              | Imm Int
+             | Label String
              | Plus Operand Operand
 
 instance Show Operand where
     show (Reg r) = "r" ++ (show r)
     show (Imm i) = show i
+    show (Label l) = l
     show (Plus r@Reg{} i@Imm{}) = "(" ++ (show r) ++ " + " ++ (show i) ++ ")"
 
 ret = Reg 0 -- Return Value
@@ -34,10 +37,29 @@ data Instruction = Push Operand
                  | Sub Operand Operand                   
                  | Mov Operand Operand
                  | Asp Operand
+                 | LabelDef String
                  | Store Operand Operand
-                 | Label String
+                 | Rsp Operand
+                 | Call Operand
+                 | Ret
                  | Nop
                  deriving Show
+
+-- Determine how much a single instruction changes the stack size
+stackChange :: Instruction -> Int
+stackChange Push{} = 1
+stackChange Pop{} = -1
+stackChange Call{} = 1
+stackChange Ret{} = -1
+stackChange (Asp (Imm x)) = negate x                    
+stackChange _ = 0
+                            
+-- Determine how much the stack grows or shrinks after executing a
+-- list of instructions
+-- Positive values are the stack growing
+-- Negative values are the satck shrinking
+stackDelta :: [Instruction] -> Int
+stackDelta = sum . (map stackChange)
 
 -- Push the accumulator onto the stack, making room for an expression to be evaluated
 pushDown = [Push acc]
@@ -47,30 +69,66 @@ binOpFunc "+" = Add
 binOpFunc "-" = Sub
 binOpFunc x = error $ "Uncrecognized Operator: " ++ x                
 
+-- The minimum number of stack words a sequence of expressions will require to execute
+stackSize :: [Expr] -> Int
+stackSize exprs = sum $ map exprSize exprs
+    
+exprSize :: Expr -> Int
+exprSize VariableDecl{} = 1
+exprSize _ = 0
+                  
+funcEntry :: Expr -> [Instruction]
+funcEntry (FuncDef _ _ _ body) =
+    [Push cfp, -- Save the current frame pointer
+     Rsp cfp, -- Calculate the new frame pointer
+     Asp ((Imm . negate . stackSize) body)] -- Make stack room for the function
+
+funcExit :: Expr -> [Instruction]
+funcExit (FuncDef _ _ _ body) =
+    [Asp ((Imm . stackSize) body),
+     Pop cfp,
+     Ret]
+
+-- Get the stack size required to pass all function arguments    
+argStackSize :: [Symbol] -> Int
+argStackSize args = length args
+
 codeGen :: Expr -> [Instruction]
 codeGen = codeGen' []
-                
+
 codeGen' :: FrameOffsetTable -> Expr -> [Instruction]
 codeGen' _   (Const v) = [Mov acc (Imm v)]
-codeGen' fot (BinOp "=" (Identifier dest) src) = (codeGen' fot src) ++
-                                                  [Store acc (cfp `Plus` ((Imm . fromJust) $ lookup dest fot))]
-codeGen' fot (BinOp "=" dest src) = (codeGen' fot dest) ++
-                                     pushDown ++
-                                     (codeGen' fot src) ++
-                                     [Pop tmp, Store acc tmp]
-codeGen' fot (BinOp op left right) = (codeGen' fot left) ++
-                                pushDown ++
-                                (codeGen' fot right) ++
-                                [Pop tmp, binOpFunc op acc tmp]
-codeGen' _   (VariableDecl _) = [Asp (Imm 1)] -- Make room for the variable
+codeGen' fot (BinOp "=" (Identifier dest) src) =
+    (codeGen' fot src) ++
+    [Store acc (cfp `Plus` ((Imm . fromJust) $ lookup dest fot))]
+codeGen' fot (BinOp "=" dest src) =
+    (codeGen' fot dest) ++
+    pushDown ++
+    (codeGen' fot src) ++
+    [Pop tmp, Store acc tmp]
+codeGen' fot (BinOp op left right) =
+    (codeGen' fot left) ++
+    pushDown ++
+    (codeGen' fot right) ++
+    [Pop tmp, binOpFunc op acc tmp]
+codeGen' _   (VariableDecl _) = []
 codeGen' [] f@(FuncDef name _ args body) =
-    [Label name, Asp ((Imm . length) args)] ++ -- TODO: Size of arguments?
-    (concatMap (codeGen' (generateFunctionOffset f)) body) ++
-    [Label ("DBG_" ++ name ++ "_end")]
-codeGen' _ _ = []    
-    
-                                     
+    [LabelDef name] ++
+     funcEntry f ++
+     (concatMap (codeGen' (generateFunctionOffset f)) body) ++
+     funcExit f++
+    [LabelDef ("DBG_" ++ name ++ "_end")]
+codeGen' fot f@(FuncCall name args) =
+    pushArgs ++
+    -- Go to the function
+    [Call (Label name),
+    -- Clean up the stack after returning
+     Asp ((Imm . stackDelta) pushArgs)]
+                      -- Evaluate all args, and push them onto the stack
+    where pushArgs = ((intercalate pushDown) $ map (codeGen' fot) args) ++ pushDown
+          
 codeGen' _ Pass = [Nop]
+codeGen' _ _ = []    
 
 emitASM :: Instruction -> Either String String
 emitASM (Push r@Reg{}) = Right $ "push " ++ (show r)
@@ -81,7 +139,10 @@ emitASM (Mov rd@Reg{} i@Imm{})  = Right $ "mov " ++ (show rd) ++ ", " ++ (show i
 emitASM (Asp i@Imm{})           = Right $ "asp " ++ (show i)
 emitASM (Store src@Reg{} dest@Reg{}) = Right $ "st " ++ (show src) ++ ", " ++ (show dest)
 emitASM (Store src@Reg{} dest@(Plus Reg{} Imm{})) = Right $ "st " ++ (show src) ++ ", " ++ (show dest)
-emitASM (Label lbl)             = Right $ lbl ++ ":"                                  
+emitASM (LabelDef lbl)             = Right $ "lbl" ++ ":"
+emitASM (Rsp r@Reg{})           = Right $ "rsp " ++ (show r)
+emitASM (Call l@Label{})          = Right $ "call " ++ show l
+emitASM Ret                     = Right "ret"
 emitASM Nop                     = Right "nop"
 emitASM instr = Left $ "Undefined Instruction: " ++ (show instr)
 
--- FrameOffsetTable.hs
+++ FrameOffsetTable.hs
@@ -6,17 +6,37 @@ type Offset = Int
     
 type Scope = [(SymbolName, Offset)]
 type FrameOffsetTable = Scope
+
+-- Stack Layout for Function Call:
+
+-- LOW ADDRESSES
+
+--  Scope Variable n
+--  Scope Variable 1
+--  Scope Variable 0
+-- Last Frame Pointer        <-  Current Frame Pointer
+--   Return Address
+--       Arg n
+--       Arg 1
+--       Arg 0
+--        ...
+---Bottom of Stack---
+-- HIGH ADDRESSES
+
+-- Distance in stack words from the frame pointer to the last argument
+distFPToLastArg = 2
     
 generateScope :: Offset -> [Expr] -> Scope
-generateScope i (e:es) = newSymbols ++ (generateScope (i + offset) es)
-  where calcOffset (VariableDecl (Symbol symName _)) = [(symName, i + 1)]
+generateScope i (e:es) = newSymbols ++ (generateScope (i - offset) es)
+  where calcOffset (VariableDecl (Symbol symName _)) = [(symName, i - 1)]
         calcOffset _ = []
         newSymbols = calcOffset e
         offset = length newSymbols
 generateScope i [] = []
                      
 generateFunctionOffset :: Expr -> Scope
-generateFunctionOffset (FuncDef _ _ vars body) = generateScope 0 exprs
+generateFunctionOffset (FuncDef _ _ vars body) = (generateScope 0 body) ++ (generateScope argOffset varDecls)
   where varDecls = map VariableDecl vars
-        exprs = varDecls ++ body
+        -- Offset of first argument.  Arguments grow toward low addresses
+        argOffset = (length vars) + distFPToLastArg
                      
